6ba6737dfbca66d18774e2d326459a8ae4a763a4
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-01-05 00:38:53 +0100

    Add tokenizer

---
+++ src/com/jsheets/util/StringTokenizer.java
@@ -0,0 +1,43 @@
+package com.jsheets.util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class StringTokenizer {
+  public static List<String> splitKeepingTokens(
+    String expression,
+    List<String> delimiters
+  ) {
+    var result = List.of(expression);
+    for (final var delimiter : delimiters) {
+      result = split(result, delimiters, delimiter);
+    }
+
+    return result;
+  }
+
+  private static List<String> split(
+    List<String> expression,
+    List<String> delimiters,
+    String delimiter
+  ) {
+    return expression
+      .stream()
+      .flatMap(e -> {
+        return delimiters.contains(e)
+          ? List.of(e).stream()
+          : splitKeepingToken(e, delimiter).stream();
+      })
+      .toList();
+  }
+
+  public static List<String> splitKeepingToken(String expression, String token) {
+    final var keepDelimiter = "((?<=%1$s)|(?=%1$s))";
+    final var result = expression.split(
+      String.format(keepDelimiter, Pattern.quote(token))
+    );
+
+    return Arrays.asList(result);
+  }
+}
