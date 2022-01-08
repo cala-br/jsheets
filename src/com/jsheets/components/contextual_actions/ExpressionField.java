aa3bcdf79fd909761905bea722048c8ac8649a08
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-01-08 20:09:42 +0100

    Change cell value; closes #16

--- src/com/jsheets/components/contextual_actions/ExpressionField.java
+++ src/com/jsheets/components/contextual_actions/ExpressionField.java
@@ -2,14 +2,23 @@ package com.jsheets.components.contextual_actions;
 
 import javax.swing.JTextField;
 
+import com.jsheets.components.misc.EnterKeyAdapter;
 import com.jsheets.components.worksheet.CellSelectionEvent;
+import com.jsheets.services.ServiceRepository;
 
 public class ExpressionField extends JTextField {
+  private final EnterKeyAdapter keyAdapter = new EnterKeyAdapter();
+  private CellSelectionEvent currentEvent = null;
+
   public ExpressionField() {
     super();
+    addKeyListener(keyAdapter);
+    keyAdapter.onEnterPressed.subscribe(this::onEnterPressed);
   }
 
   public void setText(CellSelectionEvent e) {
+    currentEvent = e;
+
     final var text = e.hasSingleCell
       ? e.data.get(0).getExpression()
       : e.data.onlyWithValue().toString();
@@ -17,8 +26,26 @@ public class ExpressionField extends JTextField {
     setText(text);
   }
 
+
+  private void onEnterPressed() {
+    if (currentEvent == null || !currentEvent.hasSingleCell) {
+      return;
+    }
+
+    final var row = currentEvent.rows[0];
+    final var col = currentEvent.columns[0];
+    final var worksheet = ServiceRepository
+      .worksheetManager
+      .getCurrentlyActive();
+
+    worksheet.setValueAt(getText(), row, col);
+    worksheet.updateUI();
+  }
+
+
   @Override
-  public void setText(String value) {
-    super.setText(value);
+  public void removeNotify() {
+    super.removeNotify();
+    keyAdapter.onEnterPressed.unsubscribe(this::onEnterPressed);
   }
 }
