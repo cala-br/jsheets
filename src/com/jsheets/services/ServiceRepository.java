8d4126e1d70ac2bf32e78c968d61c888dee14516
Stefano Calabretti <StefanoCalabretti@outlook.it>
2022-01-13 10:38:42 +0100

    Create session service

--- src/com/jsheets/services/ServiceRepository.java
+++ src/com/jsheets/services/ServiceRepository.java
@@ -1,12 +1,14 @@
 package com.jsheets.services;
 
 import com.jsheets.services.font.FontService;
+import com.jsheets.services.session.SessionService;
 import com.jsheets.services.storage.FileStorageService;
 import com.jsheets.services.storage.StorageService;
 import com.jsheets.services.worksheet_manager.WorksheetManagerService;
 
 public class ServiceRepository {
   public final static FontService fontService = new FontService();
+  public final static SessionService sessionService = new SessionService();
   public final static StorageService storageService = new FileStorageService();
   public final static WorksheetManagerService worksheetManager = new WorksheetManagerService();
 }
